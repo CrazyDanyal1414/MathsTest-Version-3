966d677735aafe7ab1a9913b7ab75ad5f679dd0e
danyal <danyal.qureshi1414@gmail.com>
2020-03-05 09:57:48 +0800

    refactoring code again

--- Program.cs
+++ Program.cs
@@ -8,103 +8,85 @@ namespace mathstester
         {
             Console.WriteLine("What difficulty level would you like to do! Please type E for Easy, N for Normal and H for hard");
             string userDifficulty = Console.ReadLine();
-            if (userDifficulty == "E")
+
+            Console.Write("How many questions would you like to answer? ");
+            int numberOfQuestions = Convert.ToInt32(Console.ReadLine());
+            int numberOfQuestionsLeft = numberOfQuestions;
+
+            Random random = new Random();
+            int score = 0;
+
+            while (numberOfQuestionsLeft > 0)
             {
-                Console.Write("How many questions would you like to answer? ");
-                int numberOfEasyQuestions = Convert.ToInt32(Console.ReadLine());
-                int numberOfEasyQuestionsLeft = numberOfEasyQuestions;
-
-                Random easyRandom = new Random();
-                int easyNumber1 = easyRandom.Next(10);
-                int easyNumber2 = easyRandom.Next(10);
-                int easyScore = 0;
-
-                while (numberOfEasyQuestionsLeft > 0)
+                if (userDifficulty == "E")
                 {
+                    int easyNumber1 = random.Next(10);
+                    int easyNumber2 = random.Next(10);
+
                     Console.Write($"What is {easyNumber1} * {easyNumber2} =");
                     int easyCorrectAnswer = easyNumber1 * easyNumber2;
                     int easyUserAnswer = Convert.ToInt32(Console.ReadLine());
                     if (easyCorrectAnswer == easyUserAnswer)
                     {
                         Console.WriteLine("Well Done!");
-                        easyScore++;
+                        score++;
                     }
                     else
                     {
                         Console.WriteLine("Your answer is incorrect!");
                     }
-                    numberOfEasyQuestionsLeft--;
-                    easyNumber1 = easyRandom.Next(10);
-                    easyNumber2 = easyRandom.Next(10);
+                    numberOfQuestionsLeft--;
+                    easyNumber1 = random.Next(10);
+                    easyNumber2 = random.Next(10);
+
                 }
-                Console.WriteLine($"You got a score of {easyScore} out of {numberOfEasyQuestions}");
-            }
-            else if (userDifficulty == "N")
-            {
-                Console.Write("How many questions would you like to answer? ");
-                int numberOfNormalQuestions = Convert.ToInt32(Console.ReadLine());
-                int numberOfNormalQuestionsLeft = numberOfNormalQuestions;
-
-                Random normalRandom = new Random();
-                int normalNumber1 = normalRandom.Next(100);
-                int normalNumber2 = normalRandom.Next(100);
-                int normalScore = 0;
-
-                while (numberOfNormalQuestionsLeft > 0)
+                else if (userDifficulty == "N")
                 {
+                    int normalNumber1 = random.Next(100);
+                    int normalNumber2 = random.Next(100);
+
                     Console.Write($"What is {normalNumber1} * {normalNumber2} =");
                     int normalCorrectAnswer = normalNumber1 * normalNumber2;
                     int normalUserAnswer = Convert.ToInt32(Console.ReadLine());
                     if (normalCorrectAnswer == normalUserAnswer)
                     {
                         Console.WriteLine("Well Done!");
-                        normalScore++;
+                        score++;
                     }
                     else
                     {
                         Console.WriteLine("Your answer is incorrect!");
                     }
-                    numberOfNormalQuestionsLeft--;
-                    normalNumber1 = normalRandom.Next(100);
-                    normalNumber2 = normalRandom.Next(100);
+                    numberOfQuestionsLeft--;
+                    normalNumber1 = random.Next(100);
+                    normalNumber2 = random.Next(100);
                 }
-                Console.WriteLine($"You got a score of {normalScore} out of {numberOfNormalQuestions}");
-            }
-            else if (userDifficulty == "H")
-            {
-                Console.Write("How many questions would you like to answer? ");
-                int numberOfHardQuestions = Convert.ToInt32(Console.ReadLine());
-                int numberOfHardQuestionsLeft = numberOfHardQuestions;
-
-                Random hardRandom = new Random();
-                int hardNumber1 = hardRandom.Next(10, 1000);
-                int hardNumber2 = hardRandom.Next(10, 1000);
-                int hardScore = 0;
-
-                while (numberOfHardQuestionsLeft > 0)
+                else if (userDifficulty == "H")
                 {
+                    int hardNumber1 = random.Next(10, 1000);
+                    int hardNumber2 = random.Next(10, 1000);
                     Console.Write($"What is {hardNumber1} * {hardNumber2} =");
                     int hardCorrectAnswer = hardNumber1 * hardNumber2;
                     int hardUserAnswer = Convert.ToInt32(Console.ReadLine());
                     if (hardCorrectAnswer == hardUserAnswer)
                     {
                         Console.WriteLine("Well Done!");
-                        hardScore++;
+                        score++;
                     }
                     else
                     {
                         Console.WriteLine("Your answer is incorrect!");
                     }
-                    numberOfHardQuestionsLeft--;
-                    hardNumber1 = hardRandom.Next(10, 1000);
-                    hardNumber2 = hardRandom.Next(10, 1000);
+                    numberOfQuestionsLeft--;
+                    hardNumber1 = random.Next(10, 1000);
+                    hardNumber2 = random.Next(10, 1000);
                 }
-                Console.WriteLine($"You got a score of {hardScore} out of {numberOfHardQuestions}");
+                else
+                {
+                    Console.WriteLine("Sorry, This is not an option");
+                }
             }
-            else
-            {
-                Console.WriteLine("Sorry, This is not an option");
-            }
+            Console.WriteLine($"You got a score of {score} out of {numberOfQuestions}");
         }
     }
 }
