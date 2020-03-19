241a69b160f9887cf4a10f7337457eff12cd0dd2
danyal <danyal.qureshi1414@gmail.com>
2020-03-20 07:50:24 +0800

    adding methods

--- Program.cs
+++ Program.cs
@@ -4,133 +4,125 @@ namespace mathstester
 {
     class Program
     {
-        enum UserDifficulty
-        {
-            Easy,
-            Normal,
-            Hard
-        }
-        public static void Main(string[] args)
-        {
-            string userDifficulty = "";
-            do
-            {
-                Console.WriteLine("What difficulty level would you like to do! Please type E for Easy, N for Normal and H for hard");
-                userDifficulty = Console.ReadLine().ToUpper();
-            } while (userDifficulty != "E" && userDifficulty != "N" && userDifficulty != "H");
+		enum UserDifficulty
+		{
+			Easy,
+			Normal,
+			Hard
+		}
 
-            int numberOfQuestions = 0;
-            do
-            {
-                Console.Write("How many questions would you like to answer? Please type a number divisible by 10!");
-                int.TryParse(Console.ReadLine(), out numberOfQuestions);
-            } while (numberOfQuestions % 10 != 0);
+		enum MathOperation
+		{
+			Addition = 1,
+			Subtraction = 2,
+			Multiplication = 3,
+			Division = 4,
+			Power = 5,
+			SquareRoot = 6
+		}
 
-            int numberOfQuestionsLeft = numberOfQuestions;
-            Random random = new Random();
-            int score = 0;
-            int operationMin = 0;
-            int operationMax = 0;
+		public static (int operationMin, int operationMax) GetDifficulty(string input)
+		{
+			switch (input)
+			{
+				case "E":
+					return (1, 4);
+				case "N":
+					return (1, 5);
+				case "H":
+					return (3, 7);
+				default:
+					throw new Exception();
+			}
+		}
 
-            switch (userDifficulty)
-            {
-                case "E":
-                    operationMin = 1;
-                    operationMax = 4;
-                    break;
-                case "N":
-                    operationMin = 1;
-                    operationMax = 5;
-                    break;
-                case "H":
-                    operationMin = 3;
-                    operationMax = 7;
-                    break;
-            }
+		public static (string message, double correctAnswer) Calculation(int mathOperation, string userDifficulty)
+		{
+			int number1;
+			int number2;
+			Random random = new Random();
 
-            while (numberOfQuestionsLeft > 0)
-            {
-                var operation = random.Next(operationMin, operationMax); 
-                double number1 = 0;
-                double number2 = 0;
-                double correctAnswer = 0;
-                double userAnswer;
+			switch (mathOperation)
+			{
+				case 1:
+					number1 = random.Next(1000);
+					number2 = random.Next(1000);
+					return ($"{number1} + {number2}", number1 + number2);
+				case 2:
+					number1 = random.Next(1000);
+					number2 = random.Next(1000);
+					return ($"{number1} - {number2}", number1 - number2);
+				case 3:
+					number1 = userDifficulty == "E" ? random.Next(13) : random.Next(1000);
+					number2 = userDifficulty == "E" ? random.Next(13) : random.Next(1000);
+					return ($"{number1} * {number2}", number1 * number2);
+				case 4:
+					number1 = random.Next(10000);
+					number2 = random.Next(1000);
+					return ($"{number1} / {number2}", number1 / (double)number2);
+				case 5:
+					number1 = random.Next(13);
+					number2 = random.Next(5);
+					return ($"{number1} ^ {number2}", Math.Pow(number1, number2));
+				case 6:
+					number1 = random.Next(1000);
+					return ($"√{number1}", Math.Sqrt(number1));
+				default:
+					throw new Exception();
+			}
+		}
 
-                string message = "";
+		public static int GetResult(int numberOfQuestionsLeft, string userDifficulty)
+		{
+			int score = 0;
+			Random random = new Random();
+			var (operationMin, operationMax) = GetDifficulty(userDifficulty);
+			while (numberOfQuestionsLeft > 0)
+			{
+				int mathOperation = random.Next(operationMin, operationMax);
+				var (message, correctAnswer) = Calculation(mathOperation, userDifficulty);
+				if (mathOperation == 4 || mathOperation == 6)
+				{
+					Console.Write($"To the nearest integer, What is {message} =");
+				}
+				else
+				{
+					Console.Write($"What is {message} =");
+				}
+				double userAnswer = Convert.ToDouble(Console.ReadLine());
+				if (Math.Round(correctAnswer) == userAnswer)
+				{
+					Console.WriteLine("Well Done!");
+					score++;
+				}
+				else
+				{
+					Console.WriteLine("Your answer is incorrect!");
+				}
+				numberOfQuestionsLeft--;
+			}
 
-                switch (operation)
-                {
-                    case 1:
-                        number1 = random.Next(1000);
-                        number2 = random.Next(1000);
-                        correctAnswer = number1 + number2;
-                        message = $"{number1} + {number2}";
-                        break;
-                    case 2:
-                        number1 = random.Next(1000);
-                        number2 = random.Next(1000);
-                        correctAnswer = number1 - number2;
-                        message = $"{number1} - {number2}";
-                        break;
-                    case 3:
-                        if(userDifficulty == "E")
-                        {
-                            number1 = random.Next(13);
-                            number2 = random.Next(13);
-                        }
-                        else
-                        {
-                            number1 = random.Next(1000);
-                            number2 = random.Next(1000);
-                        }
-                        correctAnswer = number1 * number2;
-                        message = $"{number1} * {number2}";
-                        break;
-                    case 4:
-                        number1 = random.Next(1, 10000);
-                        number2 = random.Next(1, 1000);
-                        correctAnswer = number1 / number2;
-                        message = $"{number1} / {number2}";
-                        break;
-                    case 5:
-                        number1 = random.Next(13);
-                        number2 = random.Next(5);
-                        correctAnswer = Math.Pow(number1, number2);
-                        message = $"{number1} ^ {number2}";
-                        break;
-                    case 6:
-                        number1 = random.Next(1000);
-                        correctAnswer = Math.Sqrt(number1);
-                        message = $"√{number1}";
-                        break;
-                }
+			return score;
+		}
 
+		public static void Main(string[] args)
+		{
+			string userDifficulty = "";
+			do
+			{
+				Console.WriteLine("What difficulty level would you like to do! Please type E for Easy, N for Normal and H for hard");
+				userDifficulty = Console.ReadLine().ToUpper();
+			} while (userDifficulty != "E" && userDifficulty != "N" && userDifficulty != "H");
 
-                if (operation == 4 || operation == 6)
-                {
-                    Console.Write($"To the nearest integer, What is {message} =");
-                }
-                else
-                {
-                    Console.Write($"What is {message} =");
-                }
+			int numberOfQuestions = 0;
+			do
+			{
+				Console.WriteLine("How many questions would you like to answer? Please type a number divisible by 10!");
+				int.TryParse(Console.ReadLine(), out numberOfQuestions);
+			} while (numberOfQuestions % 10 != 0);
 
-                userAnswer = Convert.ToDouble(Console.ReadLine());
-
-                if (Math.Round(correctAnswer) == userAnswer)
-                {
-                    Console.WriteLine("Well Done!");
-                    score++;
-                }
-                else
-                {
-                    Console.WriteLine("Your answer is incorrect!");
-                }
-                numberOfQuestionsLeft--;
-
-            }
-
-            Console.WriteLine($"You got a score of {score} out of {numberOfQuestions}");
-        }
-    }
+			int score = GetResult(numberOfQuestions, userDifficulty);
+			Console.WriteLine($"You got a score of {score} out of {numberOfQuestions}");
+		}
+	}
 }
